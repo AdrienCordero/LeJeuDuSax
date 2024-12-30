7318d15d19c5be8d0e75f9bc257c98808835a3cf
Adrien Cordero <cordero@insa-toulouse.fr>
2024-12-30 15:48:54 +0100

    Correction bug taille image + organisation

--- fight.js
+++ fight.js
@@ -8,7 +8,7 @@ const typeEffectiveness = {
     "fighting":{ "normal": 2, "fire": 1, "water": 1, "electric": 1, "grass": 1, "ice": 2, "fighting": 1, "poison": 1, "ground": 1, "flying": 0.5, "psychic": 0.5, "bug": 0.5, "rock": 2, "ghost": 0, "dragon": 1, "dark": 2, "steel": 2, "fairy": 0.5 },
     "poison":  { "normal": 1, "fire": 1, "water": 1, "electric": 1, "grass": 2, "ice": 1, "fighting": 1, "poison": 0.5, "ground": 0.5, "flying": 1, "psychic": 2, "bug": 1, "rock": 1, "ghost": 2, "dragon": 1, "dark": 1, "steel": 0, "fairy": 2 },
     "ground":  { "normal": 1, "fire": 2, "water": 2, "electric": 2, "grass": 0.5, "ice": 1, "fighting": 1, "poison": 2, "ground": 1, "flying": 0, "psychic": 1, "bug": 1, "rock": 2, "ghost": 1, "dragon": 1, "dark": 1, "steel": 2, "fairy": 1 },
-    //"flying":  { "normal": 1, "fire": 1, "water": 1, "electric": 0.5, "grass": 2, "ice": 1, "fighting": 2, "poison": 1, "ground": 1, "flying": 1, "psychic": 1, "bug": 2, "rock": 0.5, "ghost": 1, "dragon": 1, "dark": 1, "steel": 1, "fairy": 1 },
+    "flying":  { "normal": 1, "fire": 1, "water": 1, "electric": 0.5, "grass": 2, "ice": 1, "fighting": 2, "poison": 1, "ground": 1, "flying": 1, "psychic": 1, "bug": 2, "rock": 0.5, "ghost": 1, "dragon": 1, "dark": 1, "steel": 1, "fairy": 1 },
     "psychic": { "normal": 1, "fire": 1, "water": 1, "electric": 1, "grass": 1, "ice": 1, "fighting": 2, "poison": 2, "ground": 1, "flying": 1, "psychic": 0.5, "bug": 1, "rock": 1, "ghost": 1, "dragon": 1, "dark": 0, "steel": 1, "fairy": 1 },
     "bug":     { "normal": 1, "fire": 0.5, "water": 1, "electric": 1, "grass": 2, "ice": 1, "fighting": 0.5, "poison": 1, "ground": 1, "flying": 0.5, "psychic": 2, "bug": 1, "rock": 1, "ghost": 0.5, "dragon": 1, "dark": 2, "steel": 0.5, "fairy": 0.5 },
     "rock":    { "normal": 1, "fire": 2, "water": 1, "electric": 1, "grass": 1, "ice": 2, "fighting": 1, "poison": 1, "ground": 1, "flying": 2, "psychic": 1, "bug": 2, "rock": 1, "ghost": 1, "dragon": 1, "dark": 1, "steel": 0.5, "fairy": 1 },
@@ -287,7 +287,7 @@ function buttonPokemon(pokemon, pokemons) {
     for(let i=0; i < 3; i++) {
         if (pokemons[i].isAlive && pokemon.name != pokemons[i].name) {
             document.getElementById("changerPokemon" + i).style.display = "";
-            document.getElementById("imagePokemonAttack" + i).src = "Images/" + pokemons[i].name + ".png";
+            document.getElementById("imagePokemonAttack" + i).src = "Images/btn/" + pokemons[i].name + ".png";
             if (pokemons[i].name == "CarapuceSax")
                 document.getElementById("nomChangerPokemon" + i).textContent = "Carapuce";
             else
@@ -307,7 +307,7 @@ function refreshButtons(p) {
 
     for (let j=0; j < pokemon.attacks.length; j++) {
     	document.getElementById("attack" + (j+1)).hidden = false;
-        document.getElementById("imageAttack" + (j + 1)).src = "Images/" + pokemon.attacks[j].type + ".png";
+        document.getElementById("imageAttack" + (j + 1)).src = "Images/types/" + pokemon.attacks[j].type + ".png";
         document.getElementById("nomAttack" + (j + 1)).textContent = pokemon.attacks[j].name;
     }
     for (let j=pokemon.attacks.length; j < 4; j++){
